410a5de1c3747c426edd043d54d86b4071049440
sumukhpshetty25 <1rn22ad48.sumukhpshetty@gmail.com>
2025-03-17 16:22:04 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+    $(".w3-button").on("click", function() {
+        $(this).toggleClass("active");
+    });
+
+    showSlides(slideIndex);
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
+
+function toggleFunction() {
+    var x = document.getElementById("navSmall");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function validateForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (name && email && message) {
+        window.open('thanx.html');
+    } else {
+        alert('Please fill out all fields before submitting.');
+    }
+}
